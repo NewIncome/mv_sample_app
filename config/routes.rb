c9f44f0712e6309025429b99b74c889d01e0559d
NewIncome <alfredo_cardenas@live.com>
2020-01-23 16:09:22 -0600

    Change the root route and Finish Static Pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
-  get 'foo/baz'
+  #get 'foo/bar' rails destroy didn't delete these 2 lines...
+  #get 'foo/baz'
   get 'static_pages/home'
   get 'static_pages/help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'static_pages/about'
   get 'static_pages/contact'
-root 'application#hello'
+root 'static_pages#home'
 end
