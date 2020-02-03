01b713bda05601250cce653592550ecc2ac71e32
NewIncome <alfredo_cardenas@live.com>
2020-02-03 15:57:35 -0600

    Add link paths in the html link calls

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   #get 'static_pages/home'
-  get '/help', to: 'static_pages#help', as:'helf'
+  get '/help', to: 'static_pages#help'#, as:'helf'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
