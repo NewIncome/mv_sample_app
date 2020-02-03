ecd1870eb2a24eb0241f785a3f649c081260e4e9
NewIncome <alfredo_cardenas@live.com>
2020-02-03 14:22:02 -0600

    Edit routes to make slim route names

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  #get 'foo/bar' rails destroy didn't delete these 2 lines...
-  #get 'foo/baz'
-  get 'static_pages/home'
-  get 'static_pages/help'
+  #get 'static_pages/home'
+  get '/help' to: 'static_pages#help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get '/about' to: 'static_pages#about'
+  get '/contact' to: 'static_pages/contact'
 root 'static_pages#home'
 end
