e8e3cc7aed24159fadc1e325e83da92980c09389
NewIncome <alfredo_cardenas@live.com>
2020-02-03 14:25:43 -0600

    Fix syntax error, missing comma & miss a '/' for a '#'

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   #get 'static_pages/home'
-  get '/help' to: 'static_pages#help'
+  get '/help', to: 'static_pages#help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/about' to: 'static_pages#about'
-  get '/contact' to: 'static_pages/contact'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
 root 'static_pages#home'
 end
