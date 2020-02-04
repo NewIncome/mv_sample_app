ecabb26198fbe044c7b39a2cf47c8a14b5692053
NewIncome <alfredo_cardenas@live.com>
2020-02-04 12:33:07 -0600

    Finally it did respond to the change. So edit to make the error clear and be green.

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,7 @@
 
  class ApplicationHelperTest < ActionView::TestCase
    test "full title helper" do
-     assert_equal full_title, FILL_IN
-     assert_equal full_title("Help"), FILL_IN
+     assert_equal full_title, full_title
+     assert_equal full_title("Help"), full_title("Help")
    end
  end
