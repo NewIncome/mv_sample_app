82829eb2aa2bd1c360d86a869be75692d28b1593
NewIncome <alfredo_cardenas@live.com>
2020-02-10 16:23:45 -0600

    Add a Users resource to the routes file. This to enable all the RESTful Users resource actions and routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get '/signup', to: 'users#new'
   #get 'static_pages/home'
   get '/help', to: 'static_pages#help'#, as:'helf'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-root 'static_pages#home'
+  resources :users # endows our sample application with all the actions needed for a RESTful Users resource.
 end
