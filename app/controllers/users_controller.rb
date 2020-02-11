5759a2bf32a9aad901606ecb1ee6f101a2d49fd6
NewIncome <alfredo_cardenas@live.com>
2020-02-11 15:15:47 -0600

    Add a new controller method to privatize params and make accesible only the required attributes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,4 +18,10 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
