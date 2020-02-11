de1eecb01173c73bc6bca0b21d6e046e95d03006
NewIncome <alfredo_cardenas@live.com>
2020-02-11 15:02:57 -0600

    Add changes for the beginning form in the Sign-up page: - The ERB for the html form tag, - the css for the sign-up form, - and the controller code to add the 'new' user in the 'new' method also the 'create' method to render a succesful save view or rerender 'new'.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,16 @@ class UsersController < ApplicationController
   end
 
   def new
+    # now we'll start making new users.
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #Handle a successful save.
+    else
+      render 'new'
+    end
   end
 end
