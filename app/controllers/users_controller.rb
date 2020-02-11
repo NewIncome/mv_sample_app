0f0a427bf86cd2f362b1fcbeeab6647984ae8be0
NewIncome <alfredo_cardenas@live.com>
2020-02-11 09:52:18 -0600

    Add debugger command to use the bugging gem 'byebug'. But needs not be there if it is not going to be used.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    # debugger. Adds a debugger in the Rails Server
+    # for the chosen View.
   end
 
   def new
