a45b110ceba47816c99b6ff117ee8895b4d70260
NewIncome <alfredo_cardenas@live.com>
2020-02-19 12:04:42 -0600

    Add line to have something to see if the session authentication succeeds.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,9 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
+      render :html => "Good Job #{user.name}!!"
     else
-      # Create an error message
+      flash[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
