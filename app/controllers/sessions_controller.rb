b828a1b0c2fecfc9494a110ff1e0ccbca6626678
NewIncome <alfredo_cardenas@live.com>
2020-02-19 15:18:52 -0600

    - Add code to render correctly after login, - Add a session_helper file for method reuse, - And include the SessionsHelper module in the application_controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
-      render :html => "Good Job #{user.name}!!"
+      log_in user
+      redirect_to user   # same as-->  user_url(user)
+      debugger
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
