52ca4ebc55e760c15572589f4cd0ad05e18a7202
NewIncome <alfredo_cardenas@live.com>
2020-02-20 12:33:06 -0600

    Add the actions for the log_out method in the sessions_helper, Add the call to the method and correct redirect to root in the sessions_controller, And add the extra assertions to test the logout function with it's corresponding added features like: - not show the 'profile' or 'log out' links, - show the 'login' link, - verify the user_id was deleted.

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -21,15 +21,28 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
-  test 'login with valid information' do
+  test 'login with valid information followed by logout' do
     get login_path
     post login_path, params: { session: { email:    @user.email,
                                           password: 'password' } }
+    assert is_logged_in?
     assert_redirected_to @user   # to check the right redirect target
     follow_redirect!             # to actually visit the target page
     assert_select "a[href=?]", login_path, count: 0  # verifies the
     # login link disappears by verifying there are 0 login path links.
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+
+    # no to verify the correct logout and:
+    # correct redirect to root,
+    # correct login link show,
+    # correct NOT show of logout and profile links.
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end  
 end
