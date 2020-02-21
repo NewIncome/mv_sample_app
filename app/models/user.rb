760793acbfabc4190902e4f5784017465066a975
NewIncome <alfredo_cardenas@live.com>
2020-02-21 10:44:11 -0600

    Add new column for remember_digest, Add code to make the remember_token And add a method to add that token to the table. Thus rails added the filein migrate, to edit the table.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 19 }
   # () and {} not needed, but indeed an object is being passed.
@@ -17,5 +18,15 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
