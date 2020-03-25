5eb035967b967fa5bdd5b6a057987cf4745bc52b
NewIncome <alfredo_cardenas@live.com>
2020-03-24 18:39:37 -0600

    Add code and test to be able to correctly see email previews and test them. Also add some code fixes in the config/env for tests.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,6 +8,8 @@ class UserMailer < ApplicationMailer
   def account_activation(user)
     @user = user
     mail to: user.email, subject: 'Account activation'
+    # this is a method:
+    # mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
